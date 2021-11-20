c5e75f78479cc091b7e44d34ef04c056100fe9b5
LeonieBuschbeck <leonie.buschbeck@gmail.com>
2021-11-20 16:35:37 +0100

    Create types

---
+++ src/app/types.ts
@@ -0,0 +1,12 @@
+export type WeatherCards = {
+    id: number,
+    city: string,
+    todayTemp: string,
+    todayTempMin: string,
+    todayTempMax: string,
+    todayRain: boolean,
+    seven_DaysTemp: string,
+    seven_DaysTempMin: string,
+    seven_DaysTempMax: string,
+    seven_DaysRain: boolean,
+}
