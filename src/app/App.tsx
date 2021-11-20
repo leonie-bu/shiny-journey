c425df81f2b88d70a80865e93225730555813cd0
LeonieBuschbeck <leonie.buschbeck@gmail.com>
2021-11-20 14:25:49 +0100

    Start single landing page for weather app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,16 @@
-import React, { useState }  from 'react';
-import styles from './App.module.css';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import TodoForm from './components/TodoForm/TodoForm';
+import Weather from './pages/WeatherPage';
 
 function App(): JSX.Element {
-  const [todo, setTodo] = useState("");
 
   return (
     <BrowserRouter>
-      <div className={styles.App}>
-        <header className={styles['App-header']}>
           <Switch>
-            <Route path="/about">
-              <main>About</main>
-            </Route>
             <Route path="/">
-              <main>Home</main>
-              <TodoForm />
-
+              <Weather />
             </Route>
           </Switch>
-        </header>
-      </div>
     </BrowserRouter>
   );
 }
