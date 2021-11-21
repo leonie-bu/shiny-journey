bb9991373cf7b93f1d4afe56bfb626263f8bf766
LeonieBuschbeck <leonie.buschbeck@gmail.com>
2021-11-21 19:21:59 +0100

    Remove unnecessary files

--- src/app/components/TodoCard/TodoCard.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import TodoCard from "./TodoCard";
-
-export default {
-    title: 'Component/TodoCard',
-    component: TodoCard,
-  };
-
-  export const TodoC =() :JSX.Element => <TodoCard value="Todo" />
