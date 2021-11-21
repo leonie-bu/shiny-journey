4e1e67287b5dfc592f6bc4fb80543fb0f2471598
LeonieBuschbeck <leonie.buschbeck@gmail.com>
2021-11-21 10:51:49 +0100

    Experiment on page

--- src/app/pages/WeatherPage.tsx
+++ src/app/pages/WeatherPage.tsx
@@ -1,18 +1,37 @@
 import React, { useState} from "react";
 import styles from './WeatherPage.module.css';
 import SearchField from "../components/searchField/searchField";
+import useWeatherCard from "../hooks/useWeather";
+import { useParams } from "react-router";
+import { WEATHER } from "../lib/weather";
+import WeatherCard from "../components/weatherCard/weatherCard";
 
 export default function Weather():JSX.Element {
     const[city, setCity] = useState('');
+    const { addWeatherCard } = useWeatherCard();
+
+    function addWeatherCard() {
+        const weatherCard = {
+            city: WEATHER.city
+        }
+    }
+
+    const { id }: { id:string } = useParams();
+
+    const filteredWeather = WEATHER.filter((weather) => weather.id === Number(id));
 
     function handleChange(value:string){
         setCity(value);
     };
 
     return(
-        <form onSubmit={(event) => event.preventDefault()} className={styles.container}>
-            <SearchField  value={city} onChange={(value:string) => handleChange(value)} />
-        </form>
-        
+        <section>
+            <form onSubmit={(event) => event.preventDefault()} className={styles.container}>
+                <SearchField  value={city} onChange={(value:string) => handleChange(value)} />
+            </form>
+            <section>{filteredWeather.map((weather) => (
+                <WeatherCard key={weather.id} {...weatherCard})
+            </section>
+    </section>
     );
 }
