8d78d883ef0bfb75cb3cc0a2b728f4a7d08b542a
LeonieBuschbeck <leonie.buschbeck@gmail.com>
2021-11-22 21:20:16 +0100

    Create useFetch hook

---
+++ src/app/hooks/useFetch.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch<T>(url:string): {
+    weather: T | null;
+    handleClick: () => void;
+} {
+    const [weather, setWeather] = useState<T | null>(null);
+
+    function handleClick() {
+        fetch(url)
+            .then((res) => {
+                if (res.ok) {
+                    console.log(res.status);
+                    return res.json();
+                } else {
+                    if (res.status === 404) {
+                        return alert("Oops, there seems to be an error because of a wrong location.");
+                    }
+                    alert("Oops, there seems to be an error.");
+                    throw new Error("You have an error")
+                }
+            })
+            .then((object) => {
+                setWeather(object);
+                console.log(weather);
+            })
+            .catch((error) => console.log(error));
+        }
+        useEffect(() => {
+            handleClick();
+        }, [] //Check if something should be added to array. If sth is added --> make handleClick function an async&await function?
+        );
+        return { weather, handleClick };
+}
